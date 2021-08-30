ea48dde0603e3e39d158225ddb39594b96c521cf
Гомбоев Жаргал Баирович <gomboevz@gmail.com>
2021-08-30 16:35:30 +0300

    feat: promo cards

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,14 +2,19 @@ import poster1 from '../../images/1.jpg';
 import poster2 from '../../images/6.jpg';
 import poster3 from '../../images/11.jpg';
 import poster4 from '../../images/7.jpg';
-import PromoCard from '../PromoCard/PromoCard';
 
 function Promo() {
   return (
     <section className="promo">
-      <PromoCard />
-      {/* <div className="promo__card promo__card_main" style={{ backgroundImage: `url(${poster1})`}}></div> */}
-      <div className="promo__card promo__card_top-right" style={{ backgroundImage: `url(${poster2})`}}></div>
+      <div className="promo__card promo__card_main" style={{ backgroundImage: `url(${poster1})`}}>
+        <h2 className="promo__card-title promo__card-title_main">Главный герой</h2>
+        <p className="promo__card-text">У сотрудника крупного банка всё идёт по накатанной, пока однажды он не выясняет, что окружающий его мир — это часть огромной видеоигры, а сам он в ней — всего лишь второстепенный персонаж. Хватит ли у него духу переписать свой код, обратить на себя внимание прекрасной девушки и, наконец, спасти мир? Одним словом, получится ли из него главный герой?</p>
+        <button className="promo__card-more-button">Подробнее</button>
+      </div>
+      <div className="promo__card promo__card_top-right" style={{ backgroundImage: `url(${poster2})`}}>
+        <h2 className="promo__card-title promo__card-title_top-right">Главный герой</h2>
+        <button className="promo__card-more-button promo__card-more-button_top-right">Подробнее</button>
+      </div>
       <div className="promo__card" style={{ backgroundImage: `url(${poster3})`}}></div>
       <div className="promo__card" style={{ backgroundImage: `url(${poster4})`}}></div>
     </section>
