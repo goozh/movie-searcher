74ecf62d733b058848d677c49aab31497bdf34c7
Zh <gomboevz@gmail.com>
2021-09-15 21:20:46 +0300

    feat: card hover opacity

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -7,13 +7,17 @@ function Promo() {
   return (
     <section className="promo">
       <div className="promo__card promo__card_main" style={{ backgroundImage: `url(${poster1})`}}>
-        <h2 className="promo__card-title promo__card-title_main">Главный герой</h2>
-        <p className="promo__card-text">У сотрудника крупного банка всё идёт по накатанной, пока однажды он не выясняет, что окружающий его мир — это часть огромной видеоигры, а сам он в ней — всего лишь второстепенный персонаж. Хватит ли у него духу переписать свой код, обратить на себя внимание прекрасной девушки и, наконец, спасти мир? Одним словом, получится ли из него главный герой?</p>
-        <button className="promo__card-more-button">Подробнее</button>
+        <div className="promo__content-container">
+          <h2 className="promo__card-title promo__card-title_main">Главный герой</h2>
+          <p className="promo__card-text">У сотрудника крупного банка всё идёт по накатанной, пока однажды он не выясняет, что окружающий его мир — это часть огромной видеоигры, а сам он в ней — всего лишь второстепенный персонаж. Хватит ли у него духу переписать свой код, обратить на себя внимание прекрасной девушки и, наконец, спасти мир? Одним словом, получится ли из него главный герой?</p>
+          <button className="promo__card-more-button">Подробнее</button>
+        </div>
       </div>
       <div className="promo__card promo__card_top-right" style={{ backgroundImage: `url(${poster2})`}}>
-        <h2 className="promo__card-title promo__card-title_top-right">Воспоминания</h2>
-        <button className="promo__card-more-button promo__card-more-button_top-right">Подробнее</button>
+        <div className="promo__content-container">
+          <h2 className="promo__card-title promo__card-title_top-right">Воспоминания</h2>
+          <button className="promo__card-more-button promo__card-more-button_top-right">Подробнее</button>
+        </div>
       </div>
       <div className="promo__card" style={{ backgroundImage: `url(${poster3})`}}>
         <h2 className="promo__card-title promo__card-title_small">Дом на другой стороне</h2>
