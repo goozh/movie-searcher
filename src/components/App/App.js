ad198d7ffd3765c6b3624a5a02df4bbd4c0d3e38
Zh <gomboevz@gmail.com>
2021-09-25 21:25:45 +0300

    feat: section featured

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import Highlights from '../Highlights/Highlights';
+import Featured from '../Featured/Featured';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Promo />
         <Header isLoggedIn={true} />
         <Highlights />
+        <Featured />
       </div>
     </div>
   );
