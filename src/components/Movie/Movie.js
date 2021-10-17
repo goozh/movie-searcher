84fecf1d155ae724b82a1323f2059c5f832d8056
Zh <gomboevz@gmail.com>
2021-10-17 20:00:54 +0300

    fix: images url

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -34,7 +34,7 @@ function Movie() {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </section>
