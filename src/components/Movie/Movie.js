5895d08f33166cdcabe5694a050eda8dd4ece37d
Zh <gomboevz@gmail.com>
2021-10-17 21:46:23 +0300

    fix: card image hover

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -32,7 +32,7 @@ function Movie() {
           className="movie__trailer-iframe"
           src={MOVIES_DATA[id].trailer}
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,24 +9,28 @@ function Promo() {
   return (
     <section className="promo">
       <NavLink to="/" className="promo__logo"></NavLink>
-      <div className="promo__card promo__card_main" style={{ backgroundImage: `url(${MOVIES_DATA[0].poster})`}}>
+      <div className="promo__card promo__card_main">
         <div className="promo__content-container">
           <h2 className="promo__card-title promo__card-title_main">{MOVIES_DATA[0].title}</h2>
           <p className="promo__card-text">{MOVIES_DATA[0].sinopsys}</p>
           <NavLink to={`/movie/${MOVIES_DATA[0].id}`} className="promo__card-more-button">Подробнее</NavLink>
         </div>
+        <img className="promo__card-image" src={MOVIES_DATA[0].poster} alt={MOVIES_DATA[0].title}></img>
       </div>
-      <div className="promo__card promo__card_top-right" style={{ backgroundImage: `url(${MOVIES_DATA[1].poster})`}}>
+      <div className="promo__card promo__card_top-right">
         <div className="promo__content-container">
           <h2 className="promo__card-title promo__card-title_top-right">{MOVIES_DATA[1].title}</h2>
           <NavLink to={`/movie/${MOVIES_DATA[1].id}`} className="promo__card-more-button promo__card-more-button_top-right">Подробнее</NavLink>
         </div>
+        <img className="promo__card-image" src={MOVIES_DATA[1].poster} alt={MOVIES_DATA[1].title}></img>
       </div>
-      <div className="promo__card promo__card_small" style={{ backgroundImage: `url(${MOVIES_DATA[2].poster})`}}>
+      <div className="promo__card promo__card_small">
         <NavLink to={`/movie/${MOVIES_DATA[2].id}`} className="promo__card-title promo__card-title_small">{MOVIES_DATA[2].title}</NavLink>
+        <img className="promo__card-image" src={MOVIES_DATA[2].poster} alt={MOVIES_DATA[2].title}></img>
       </div>
-      <div className="promo__card promo__card_small" style={{ backgroundImage: `url(${MOVIES_DATA[3].poster})`}}>
+      <div className="promo__card promo__card_small">
         <NavLink to={`/movie/${MOVIES_DATA[3].id}`} className="promo__card-title promo__card-title_small">{MOVIES_DATA[3].title}</NavLink>
+        <img className="promo__card-image" src={MOVIES_DATA[3].poster} alt={MOVIES_DATA[3].title}></img>
       </div>
     </section>
   );
