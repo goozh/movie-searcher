726604e877366dad021cccab93f4181c3ea48716
Гомбоев Жаргал Баирович <gomboevz@gmail.com>
2021-10-20 17:07:31 +0300

    fix: highlight cards

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { MOVIES_DATA } from '../../utils/constants';
 
 function Highlights() {
@@ -5,21 +6,29 @@ function Highlights() {
     <section className="highlights">
       <h2 className="highlights__title">Смотрите в кино</h2>
       <ul className="highlights__container">
-        <li className="highlights__card">
-          <img className="highlights__card-image" alt={MOVIES_DATA[4].title} src={MOVIES_DATA[4].poster_min}></img>
-          <h3 className="highlights__card-title">{MOVIES_DATA[4].title}</h3>
+        <li className="highlights__list-item">
+          <NavLink to={`/movie/${MOVIES_DATA[4].id}`} className="highlights__card">
+            <img className="highlights__card-image" alt={MOVIES_DATA[4].title} src={MOVIES_DATA[4].poster_min}></img>
+            <h3 className="highlights__card-title">{MOVIES_DATA[4].title}</h3>
+          </NavLink>
         </li>
-        <li className="highlights__card">
-          <img className="highlights__card-image" alt={MOVIES_DATA[5].title} src={MOVIES_DATA[5].poster_min}></img>
-          <h3 className="highlights__card-title">{MOVIES_DATA[5].title}</h3>
+        <li className="highlights__list-item">
+          <NavLink to={`/movie/${MOVIES_DATA[5].id}`} className="highlights__card">
+            <img className="highlights__card-image" alt={MOVIES_DATA[5].title} src={MOVIES_DATA[5].poster_min}></img>
+            <h3 className="highlights__card-title">{MOVIES_DATA[5].title}</h3>
+          </NavLink>
         </li>
-        <li className="highlights__card">
-          <img className="highlights__card-image" alt={MOVIES_DATA[6].title} src={MOVIES_DATA[6].poster_min}></img>
-          <h3 className="highlights__card-title">{MOVIES_DATA[6].title}</h3>
+        <li className="highlights__list-item">
+          <NavLink to={`/movie/${MOVIES_DATA[6].id}`} className="highlights__card">
+            <img className="highlights__card-image" alt={MOVIES_DATA[6].title} src={MOVIES_DATA[6].poster_min}></img>
+            <h3 className="highlights__card-title">{MOVIES_DATA[6].title}</h3>
+          </NavLink>
         </li>
-        <li className="highlights__card">
-          <img className="highlights__card-image" alt={MOVIES_DATA[7].title} src={MOVIES_DATA[7].poster_min}></img>
-          <h3 className="highlights__card-title">{MOVIES_DATA[7].title}</h3>
+        <li className="highlights__list-item">
+          <NavLink to={`/movie/${MOVIES_DATA[7].id}`} className="highlights__card">
+            <img className="highlights__card-image" alt={MOVIES_DATA[7].title} src={MOVIES_DATA[7].poster_min}></img>
+            <h3 className="highlights__card-title">{MOVIES_DATA[7].title}</h3>
+          </NavLink>
         </li>
       </ul>
     </section>
